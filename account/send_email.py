0eea75243f055764e96177da60e8dfb3576d345b
vladislav001015 <vladislav001015@gmail.com>
2023-02-28 11:01:58 +0600

    added login logic

---
+++ account/send_email.py
@@ -0,0 +1,9 @@
+from django.core.mail import send_mail
+
+def send_activation_code(email, code):
+    send_mail(
+        'Py25 shop project', # title
+        f'http://localhost:8000/account/activate/{code}', # body
+        'vladislav001015@gmail.com', # from
+        [email] # to
+    )
