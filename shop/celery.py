298eca4ad13b79379220322b10958c3528e89743
vladislav001015 <vladislav001015@gmail.com>
2023-03-10 11:00:09 +0600

    change celery file

--- shop/celery.py
+++ shop/celery.py
@@ -6,7 +6,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "shop.settings")
 
 app = Celery("shop")
 
-app.config_from_object("django.conf.settings", namespace="CELERY2")  # CELERY_
+app.config_from_object("django.conf.settings", namespace="CELERY")  # CELERY_
 
 app.autodiscover_tasks()  # tasks
 
